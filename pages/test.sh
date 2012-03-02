44ffc983722e05ca867320b642fe3a578231636e
Tristen Russ <Playfrog4u@hotmail.com>
2012-03-02 06:36:54 -0600

    deleted

--- pages/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-siriserver_pid=`ps ax | grep siriServer | grep -v grep | awk '{print $1}'`
-echo $siriserver_pid
-
-siriserver_uptime=`ps -p $siriserver_pid -o “%t” | tail -1`
-echo $siriserver_uptime
